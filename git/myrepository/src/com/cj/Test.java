cdf5b781c86fbfba4187251b8257e68e0b90b6bd
JANMEJAYA <JANMEJAYA@BAPU>
2018-10-12 09:56:00 +0530

    4th change

--- git/myrepository/src/com/cj/Test.java
+++ git/myrepository/src/com/cj/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
-		System.out.println("Hello");
+		System.out.println("Good Morning");
 		
 		System.out.println("12.10.2018");
 
